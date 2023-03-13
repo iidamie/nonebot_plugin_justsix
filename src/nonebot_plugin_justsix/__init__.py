0dc9db50822829ab32ebc5988e5de78dd4ac3f70
tkgs0 <hgc.sakuya@ya.ru>
2023-03-13 12:16:49 +0000

    简化代码

--- src/nonebot_plugin_justsix/__init__.py
+++ src/nonebot_plugin_justsix/__init__.py
@@ -1,39 +1,10 @@
-from nonebot import on_command
-from nonebot.permission import SUPERUSER
-from nonebot.matcher import Matcher
+from nonebot import on_regex
 from nonebot.internal.adapter import Event
 
-six = on_command("6")
+
+six = on_regex(r"^6+$", priority=5, block=False)
 
 
 @six.handle()
-async def _(matcher: Matcher, _: Event):
-    await matcher.finish("6")
-
-sixxx = on_command("666")
-
-
-@sixxx.handle()
-async def _(matcher: Matcher, _: Event):
-    await matcher.finish("666")
-
-sixxxx = on_command("6666")
-
-
-@sixxxx.handle()
-async def _(matcher: Matcher, _: Event):
-    await matcher.finish("6666")
-
-sixxxxx = on_command("66666")
-
-
-@sixxxxx.handle()
-async def _(matcher: Matcher, _: Event):
-    await matcher.finish("666")
-
-sixxxxxx = on_command("666666")
-
-
-@sixxxxxx.handle()
-async def _(matcher: Matcher, _: Event):
-    await matcher.finish("666666")
+async def _(event: Event):
+    await six.finish(event.get_message())
